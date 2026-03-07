477c6b5e78108c26624c60455adb7da7f3a59b9b
長田学己 <osada@nagatagakuonorenoMacBook-Pro.local>
2026-03-08 02:08:20 +0900

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import Link from "next/link"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -60,6 +61,7 @@ export default function RootLayout({
 
         {/* 各ページ */}
         {children}
+        <Analytics />
       </body>
     </html>
   )
